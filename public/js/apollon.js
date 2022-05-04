a7f43e2abbe52e5ea7427ae99946ae68e5c1a21b
G.Bourel <gbourel@gmail.com>
2022-05-04 10:57:18 +0300

    Add achievements percent

--- public/js/apollon.js
+++ public/js/apollon.js
@@ -1,6 +1,6 @@
 (function (){
 
-const VERSION = 'v0.3.8';
+const VERSION = 'v0.3.10';
 document.getElementById('version').textContent = VERSION;
 
 let _pythonEditor = null; // Codemirror editor
@@ -333,6 +333,31 @@ function logout() {
   location.reload();
 }
 
+function updateAchievments() {
+  console.info(_user);
+  if(!_user || !_user.exercises) { return; }
+  for (let i=1; i<4 ; i++){
+    let elt = document.querySelector(`#level-${i} .percent`);
+    let total =  _user.exercises[`level${i}`];
+    let done = 0;
+    for (let r of _user.results){
+      if(r.level === i) {
+        done++;
+      }
+    }
+    let percent = 100.0 * done / total;
+    let stars = Math.round(percent/20);
+    let starsContent = '';
+    for(let i = 1; i <= 5; i++){
+      let color = 'text-gray-400';
+      if(i <= stars) { color = 'text-yellow-500'; }
+      starsContent += `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current ${color}"><path d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"></path></svg>`;
+    }
+    elt.innerHTML = `&nbsp; ${Math.round(percent)} % terminé`;
+    document.querySelector(`#level-${i} .stars`).innerHTML = starsContent;
+  }
+}
+
 function init(){
   let purl = new URL(window.location.href);
   if(purl && purl.searchParams) {
@@ -382,7 +407,7 @@ function init(){
       _user = user;
       document.getElementById('username').innerHTML = user.firstName || 'Moi';
       document.getElementById('profile-menu').classList.remove('hidden');
-      _user = user;
+      updateAchievments();
     } else {
       document.getElementById('login').classList.remove('hidden');
       _user = null;
